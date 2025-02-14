eebbb1c91ffbf5a24e00b9cc144382c2a39b9739
justemitayo <temitayoakindeju@gmail.com>
2025-02-14 07:20:45 +0100

    effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Reducer from './Reducer.tsx';
+import Effect from './Effect.tsx';
 
 const App =() => {
   return (
     <div className="App">
       <Reducer />
+      <Effect />
     </div>
   );
 }
---
+++ src/Effect.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import axios from 'axios'
+
+const Effect = () => {
+  const [data, setData] = useState('');
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/comments')
+      .then((response) => {
+        setData(response.data[0].email)
+      })
+  }, [])
+  return (
+    <div>
+      <hr />
+      Hello Word 
+      <h1>{data}</h1>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>Click</button>
+      <hr />
+    </div>
+  )
+}
+
+export default Effect
